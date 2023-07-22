7bed381e2954a9bca3ab84d90d2dcee2f7b8bcbf
HAZZA <justadollrussiashop@gmail.com>
2023-07-23 00:57:08 +0300

    add lock camera and some changes

--- Assets/Scripts/SmoothCamera.cs
+++ Assets/Scripts/SmoothCamera.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Vector3 offset = new Vector3(0f, 3f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
 
     [SerializeField] private Transform target;
 
@@ -12,5 +15,9 @@ public class CameraFollow : MonoBehaviour
     {
         Vector3 targetPosition = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+            Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+            Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
     }
 }
