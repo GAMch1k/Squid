c5eaca2afa457a0ed30c08b03e73f205d8f3fc78
HAZZA <justadollrussiashop@gmail.com>
2023-07-23 14:19:22 +0300

    fix ui

--- Assets/Scripts/ButtonPlay.cs
+++ Assets/Scripts/ButtonPlay.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonPlay : MonoBehaviour
 {
+    public Animator anim;
     public GameObject menu;
+    public GameObject ShadowMenu;
     public GameObject button1;
     public GameObject button2;
     public GameObject button3;
@@ -15,28 +18,39 @@ public class ButtonPlay : MonoBehaviour
         if (menu != null)
         {
             var animator = menu.GetComponent<Animator>();
+            var animator1 = ShadowMenu.GetComponent<Animator>();
             if (animator != null)
             {
                 animator.SetTrigger(trigger);
+                animator1.SetTrigger(trigger);
             }
         }
     }
+    private void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Pressed"))
+        {
 
+            onMenuExtendNo();
+        }
+    }
     public void onMenuExtendYes()
     {
         button1.SetActive(false);
         button2.SetActive(false);
         button3.SetActive(true);
-        ExecuteTrigger("set");
+        ExecuteTrigger("sett");
 
     }
     public void onMenuExtendNo()
     {
+        
         button1.SetActive(true);
         button2.SetActive(true);
-        button3.GetComponent<Animator>().SetTrigger("Normal");
+        button3.GetComponent<Animator>().SetTrigger("Back");
         ExecuteTrigger("def");
         button3.SetActive(false);
 
     }
+    
 }
