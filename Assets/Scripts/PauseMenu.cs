34f40750af7c963f1df437ce79a11aa4a8dd3240
GAMch1k <GAMch1k@github.com>
2023-07-24 07:25:22 +0200

    Pause menu added

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+
+    bool menu_opened = false;
+
+   
+    private void Start() {
+        HideMenu();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (menu_opened) {
+                HideMenu();
+            } else {
+                OpenMenu();
+            }
+        }
+    }
+
+    public void OpenMenu() {
+        Time.timeScale = 0;
+
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(true);
+        }
+
+        menu_opened = true;
+    }
+
+    public void HideMenu() {
+
+        Time.timeScale = 1;
+
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(false);
+        }
+
+        menu_opened = false;
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
