205cec7f1dbb6bd53fbfc65f6be70f0baa083e32
Oleksii Petrenko <minebyte@skfx.io>
2023-07-24 05:45:59 +0200

    exit (bymbaya)

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,74 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour {
+public class Door : MonoBehaviour
+{
     public GameObject[] interactables;
+    public Animator anim;
     public bool opened = false;
-    private bool opened_previous = false;
-
-    private Animator _door_stone_animator;
 
     BoxCollider2D boxCollider;
 
-    private void Start() {
+    private void Start()
+    {
         boxCollider = GetComponent<BoxCollider2D>();
-        _door_stone_animator = GetComponentInChildren<Animator>();
     }
 
-    private void Update() {
-        foreach (GameObject gobject in interactables) {
+    private void Update()
+    {
+        foreach (GameObject gobject in interactables)
+        {
 
-            if (gobject.GetComponent<Lever>() != null) {
-                if (!gobject.GetComponent<Lever>().triggered) {
+            if (gobject.GetComponent<Lever>() != null)
+            {
+                if (!gobject.GetComponent<Lever>().triggered)
+                {
                     CloseDoor();
                     break;
                 }
             }
 
-            if (gobject.GetComponent<Button>() != null) {
-                if (!gobject.GetComponent<Button>().triggered) {
+            if (gobject.GetComponent<Button>() != null)
+            {
+                if (!gobject.GetComponent<Button>().triggered)
+                {
                     CloseDoor();
                     break;
                 }
             }
 
-            if (gobject.GetComponent<Wire>() != null) {
-                if (!gobject.GetComponent<Wire>().triggered) {
+            if (gobject.GetComponent<Wire>() != null)
+            {
+                if (!gobject.GetComponent<Wire>().triggered)
+                {
                     CloseDoor();
                     break;
                 }
             }
 
             OpenDoor();
-        }   
+        }
     }
 
-    public void OpenDoor() {
-        opened = true;
+    public void OpenDoor()
+    {
         boxCollider.isTrigger = true;
-        RunAnimation();
+        anim.SetBool("Opened", true);
     }
 
-    public void CloseDoor() {
-        opened = false;
+    public void CloseDoor()
+    {
         boxCollider.isTrigger = false;
-        RunAnimation();
+        anim.SetBool("Opened", false);
     }
-
-
-    public void RunAnimation() {
-        if (opened_previous == opened) {
-            return;
-        }
-        
-        opened_previous = opened;
-
-        if (opened) {
-            _door_stone_animator.SetTrigger("Opened");
-            return;
-        }
-        _door_stone_animator.SetTrigger("Closed");
-    }
-
-
 }
